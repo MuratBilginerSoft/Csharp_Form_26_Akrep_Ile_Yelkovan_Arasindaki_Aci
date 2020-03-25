c0bc9409cb497e5cedf7b7239fc525fc167715c2
Murat Bilginer <muratbilginer09@gmail.com>
2020-03-25 22:15:11 +0300

    Initial commit

---
+++ Kod H Akrep ile Yelkovan Arasındaki Açı/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.PicSimge = new System.Windows.Forms.PictureBox();
+            this.PicKapat = new System.Windows.Forms.PictureBox();
+            this.ovalShape1 = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.Onay = new System.Windows.Forms.Button();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.GAcı = new System.Windows.Forms.GroupBox();
+            this.TxtKaci = new System.Windows.Forms.TextBox();
+            this.TxtBaci = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSimge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicKapat)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.GAcı.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.PicSimge);
+            this.panel1.Controls.Add(this.PicKapat);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(354, 36);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(43, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(220, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Akrep İle Yelkovan Arasındaki Açı";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources._01___Logo_2017;
+            this.pictureBox3.Location = new System.Drawing.Point(3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(34, 31);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // PicSimge
+            // 
+            this.PicSimge.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicSimge.BackColor = System.Drawing.Color.Transparent;
+            this.PicSimge.Image = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources._13_Simge;
+            this.PicSimge.Location = new System.Drawing.Point(284, 2);
+            this.PicSimge.Name = "PicSimge";
+            this.PicSimge.Size = new System.Drawing.Size(34, 31);
+            this.PicSimge.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicSimge.TabIndex = 3;
+            this.PicSimge.TabStop = false;
+            this.PicSimge.Click += new System.EventHandler(this.PicSimge_Click);
+            // 
+            // PicKapat
+            // 
+            this.PicKapat.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PicKapat.BackColor = System.Drawing.Color.Transparent;
+            this.PicKapat.Image = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources._10;
+            this.PicKapat.Location = new System.Drawing.Point(318, 2);
+            this.PicKapat.Name = "PicKapat";
+            this.PicKapat.Size = new System.Drawing.Size(34, 31);
+            this.PicKapat.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PicKapat.TabIndex = 2;
+            this.PicKapat.TabStop = false;
+            this.PicKapat.Click += new System.EventHandler(this.PicKapat_Click);
+            // 
+            // ovalShape1
+            // 
+            this.ovalShape1.BackgroundImage = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources.icon;
+            this.ovalShape1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ovalShape1.Location = new System.Drawing.Point(97, 11);
+            this.ovalShape1.Name = "ovalShape1";
+            this.ovalShape1.Size = new System.Drawing.Size(57, 50);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.maskedTextBox1);
+            this.groupBox1.Controls.Add(this.Onay);
+            this.groupBox1.Controls.Add(this.shapeContainer1);
+            this.groupBox1.Location = new System.Drawing.Point(2, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(350, 140);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Saat";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskedTextBox1.Location = new System.Drawing.Point(165, 44);
+            this.maskedTextBox1.Mask = "00:00";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(84, 27);
+            this.maskedTextBox1.TabIndex = 17;
+            this.maskedTextBox1.Text = "0000";
+            // 
+            // Onay
+            // 
+            this.Onay.BackgroundImage = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources.Buton;
+            this.Onay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Onay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Onay.Location = new System.Drawing.Point(100, 88);
+            this.Onay.Name = "Onay";
+            this.Onay.Size = new System.Drawing.Size(149, 35);
+            this.Onay.TabIndex = 16;
+            this.Onay.Text = "Açı Hesapla";
+            this.Onay.UseVisualStyleBackColor = true;
+            this.Onay.Click += new System.EventHandler(this.Onay_Click);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(3, 19);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.ovalShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(344, 118);
+            this.shapeContainer1.TabIndex = 18;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // GAcı
+            // 
+            this.GAcı.BackColor = System.Drawing.Color.Transparent;
+            this.GAcı.Controls.Add(this.TxtKaci);
+            this.GAcı.Controls.Add(this.TxtBaci);
+            this.GAcı.Controls.Add(this.label5);
+            this.GAcı.Controls.Add(this.label3);
+            this.GAcı.Controls.Add(this.pictureBox5);
+            this.GAcı.Controls.Add(this.pictureBox4);
+            this.GAcı.Location = new System.Drawing.Point(0, 185);
+            this.GAcı.Name = "GAcı";
+            this.GAcı.Size = new System.Drawing.Size(355, 115);
+            this.GAcı.TabIndex = 5;
+            this.GAcı.TabStop = false;
+            this.GAcı.Text = "Açı";
+            this.GAcı.Visible = false;
+            // 
+            // TxtKaci
+            // 
+            this.TxtKaci.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TxtKaci.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtKaci.Location = new System.Drawing.Point(230, 55);
+            this.TxtKaci.Multiline = true;
+            this.TxtKaci.Name = "TxtKaci";
+            this.TxtKaci.ReadOnly = true;
+            this.TxtKaci.Size = new System.Drawing.Size(114, 30);
+            this.TxtKaci.TabIndex = 15;
+            this.TxtKaci.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TxtBaci
+            // 
+            this.TxtBaci.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TxtBaci.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtBaci.Location = new System.Drawing.Point(59, 55);
+            this.TxtBaci.Multiline = true;
+            this.TxtBaci.Name = "TxtBaci";
+            this.TxtBaci.ReadOnly = true;
+            this.TxtBaci.Size = new System.Drawing.Size(114, 30);
+            this.TxtBaci.TabIndex = 14;
+            this.TxtBaci.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(251, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Küçük Açı";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(83, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Büyük Açı";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Image = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources._360_icon;
+            this.pictureBox5.Location = new System.Drawing.Point(179, 49);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(45, 43);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Image = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources._360_icon;
+            this.pictureBox4.Location = new System.Drawing.Point(10, 49);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(45, 43);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı.Properties.Resources._45__2_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(356, 313);
+            this.Controls.Add(this.GAcı);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSimge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicKapat)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.GAcı.ResumeLayout(false);
+            this.GAcı.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox PicSimge;
+        private System.Windows.Forms.PictureBox PicKapat;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape ovalShape1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button Onay;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private System.Windows.Forms.GroupBox GAcı;
+        private System.Windows.Forms.TextBox TxtKaci;
+        private System.Windows.Forms.TextBox TxtBaci;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Kod H Akrep ile Yelkovan Arasındaki Açı/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Kod_H_Akrep_ile_Yelkovan_Arasındaki_Açı
+{
+    public partial class Form1 : Form
+    {
+        #region Değişkenler
+
+        int Saat, Dakika;
+        double KAci, BAci;
+
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void PicKapat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PicSimge_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+      
+
+        private void Onay_Click(object sender, EventArgs e)
+        {
+            string[] Zaman = maskedTextBox1.Text.Split(':');
+            Saat = Convert.ToInt16(Zaman[0]) % 12;
+            Dakika = Convert.ToInt16(Zaman[1]);
+
+
+            KAci = Math.Abs((Saat * 30) - (Dakika * 6));
+            BAci = 360 - KAci;
+
+            if (BAci > KAci)
+            {
+                TxtKaci.Text = KAci.ToString();
+                TxtBaci.Text = BAci.ToString();
+            }
+
+            else
+            {
+                TxtKaci.Text = BAci.ToString();
+                TxtBaci.Text = KAci.ToString();
+            }
+
+            GAcı.Visible = true;
+        }
+
+        
+    }
+}
